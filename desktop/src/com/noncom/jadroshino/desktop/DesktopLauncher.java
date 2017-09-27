50e2902641878b0a1a8cf2776de01b663300d42e
AndrewOculus <andrewoculus@gmail.com>
2017-09-28 00:09:29 +0300

    first commit

---
+++ desktop/src/com/noncom/jadroshino/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.noncom.jadroshino.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.noncom.jadroshino.Root;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 600;
+		config.height = 800;
+		new LwjglApplication(new Root(), config);
+	}
+}
